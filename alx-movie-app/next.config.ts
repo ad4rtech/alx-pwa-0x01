dac644fda7aec33c42a4e779ec47daaf4f247039
Fidel Adar <adarfidel.ogw@gmail.com>
2025-12-17 21:30:09 +0300

    Test and Deploy Your PWA (Bonus)

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,18 +1,21 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
-
 const withPWA = withPWAInit({
-  dest: 'public'
-})
+  dest: "public",
+});
 
 const nextConfig = {
   reactStrictMode: true,
+
+  
+  turbopack: {},
+
   images: {
-    domains: ['m.media-amazon.com'],
+    domains: ["m.media-amazon.com"],
   },
 };
 
 export default withPWA({
-  ...nextConfig
-})
+  ...nextConfig,
+});
